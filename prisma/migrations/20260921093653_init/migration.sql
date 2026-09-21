33d91196082180f548736f10c9379ffbeb6ae8d0
MD. AL-AMIN <amin57.cse@gmail.com>
2026-09-21 15:37:11 +0600

    feat(db): add prisma schema for users, patients and drivers

---
+++ prisma/migrations/20260921093653_init/migration.sql
@@ -0,0 +1,107 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'DRIVER', 'PATIENT');
+
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('ACTIVE', 'BLOCKED', 'DELETED');
+
+-- CreateEnum
+CREATE TYPE "AuthProvider" AS ENUM ('GOOGLE', 'CREDENTIAL');
+
+-- CreateTable
+CREATE TABLE "drivers" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "contactNumber" TEXT,
+    "licenseNumber" TEXT NOT NULL,
+    "vehicleNumber" TEXT NOT NULL,
+    "ambulanceType" TEXT,
+    "isAvailable" BOOLEAN NOT NULL DEFAULT true,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "deletedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "drivers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "patients" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "contactNumber" TEXT,
+    "address" TEXT,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "deletedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "patients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "googleId" TEXT,
+    "authProvider" "AuthProvider" NOT NULL DEFAULT 'CREDENTIAL',
+    "emailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "role" "Role" NOT NULL DEFAULT 'PATIENT',
+    "status" "UserStatus" NOT NULL DEFAULT 'ACTIVE',
+    "needPasswordChange" BOOLEAN NOT NULL DEFAULT false,
+    "imageUrl" TEXT NOT NULL DEFAULT '',
+    "imagePublicId" TEXT NOT NULL DEFAULT '',
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "deletedAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "drivers_email_key" ON "drivers"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "drivers_licenseNumber_key" ON "drivers"("licenseNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "drivers_userId_key" ON "drivers"("userId");
+
+-- CreateIndex
+CREATE INDEX "idx_driver_email" ON "drivers"("email");
+
+-- CreateIndex
+CREATE INDEX "idx_driver_isAvailable" ON "drivers"("isAvailable");
+
+-- CreateIndex
+CREATE INDEX "idx_driver_isDeleted" ON "drivers"("isDeleted");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "patients_email_key" ON "patients"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "patients_userId_key" ON "patients"("userId");
+
+-- CreateIndex
+CREATE INDEX "idx_patient_email" ON "patients"("email");
+
+-- CreateIndex
+CREATE INDEX "idx_patient_isDeleted" ON "patients"("isDeleted");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_googleId_key" ON "users"("googleId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "drivers" ADD CONSTRAINT "drivers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "patients" ADD CONSTRAINT "patients_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
